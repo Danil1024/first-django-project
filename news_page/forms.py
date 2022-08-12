5e7f27ab7e7e339a1b707013809a586c8acd5fe2
Danil1024 <danaiek2003@mail.ru>
2022-08-12 13:44:30 +0600

    improvement CBV

--- news_page/forms.py
+++ news_page/forms.py
@@ -1,30 +1,12 @@
 from django import forms
 from .models import *
 from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-class RegistrationUser(forms.ModelForm):
-	username = forms.CharField(label='Ник')
-
-	def clean(self):
-		cleaned_data = super().clean()
-		username = cleaned_data.get('username')
-		if username in MyUser.objects.values('username'):
-			raise forms.ValidationError()
-
- 
+class RegistrationUser(UserCreationForm):
+	password1 = forms.CharField(label='пароль',required=True, widget=forms.PasswordInput)
+	password2 = forms.CharField(label='повторите пароль',required=True, widget=forms.PasswordInput)
 	class Meta:
 		model = MyUser
-		fields = ('first_name','last_name','username','password')
-
-
-class AuthenticationForm(forms.Form):
-	username = forms.CharField(label='Ник',required=True)
-	password = forms.CharField(label='Пароль', required=True, widget=forms.PasswordInput)
-	def clean(self):
-		cleaned_data = super().clean()
-		username = cleaned_data.get('username')
-		password = cleaned_data.get('password')
-		self.user = authenticate(username=username, password=password)
-		if self.user is None:
-			raise forms.ValidationError('')
+		fields = ('first_name','last_name','username')
--- news_page/views.py
+++ news_page/views.py
@@ -6,6 +6,12 @@ from .forms import *
 from .models import *
 from django.contrib.auth import login, logout
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class News_page(ListView):
@@ -14,48 +20,27 @@ class News_page(ListView):
     context_object_name = 'articles'
 
 
-def registration(request):
-    if request.method == 'POST':
-        form = RegistrationUser(request.POST)
-        if form.is_valid():
-            MyUser.objects.create_user(
-                form.cleaned_data['username'],
-                password=form.cleaned_data['password'],
-                last_name=form.cleaned_data['last_name'],
-                first_name=form.cleaned_data['first_name'],
-            )
-            return HttpResponseRedirect('/news_page/logging/')
-        else:
-            return HttpResponse('Пользователь с таком ником уже существует!')
-
-    else:
-        form = RegistrationUser()
-        return render(request, 'registration.html', {'form': form})
+class Registration(CreateView):
+    form_class = RegistrationUser
+    template_name = 'registration.html'
+    success_url = reverse_lazy('logging')
 
 
-def logging(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
-        if form.is_valid():
-            login(request, form.user)
-            return HttpResponseRedirect('/news_page/breaking_news/')
-        else:
-            return HttpResponse('Пользователь с таким ником не зарегистрирован')
-    else:
-        form = AuthenticationForm()
-        return render(request, 'logging.html', {'form': form})
+class Login(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'logging.html'
+    success_url = reverse_lazy('breaking_news')
 
 
-def logout_user(request):
-    if request.user.is_authenticated:
-        logout(request)
-        return HttpResponseRedirect('/news_page/')
+class Logout(LogoutView):
+    pass
 
 
-class Breaking_news(ListView):
+class Breaking_news(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'breaking_news.html'
     context_object_name = 'lasts_articles'
+    login_url = reverse_lazy('logging')
 
     def get_queryset(self):
         return Article.objects.filter(publication_date__gte=(timezone.now() - timedelta(1)))
